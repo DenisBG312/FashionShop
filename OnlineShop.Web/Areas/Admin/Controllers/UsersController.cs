4db373143634c4ce407a297e608696127376abb0
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2024-11-26 23:07:40 +0200

    created user controller in admin

---
+++ OnlineShop.Web/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Data.Models;
+using OnlineShop.Web.Areas.Admin.Models;
+
+namespace OnlineShop.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = _userManager.Users.ToList();
+
+            var userViewModels = new List<UserViewModel>();
+
+            foreach (var user in users)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+
+                userViewModels.Add(new UserViewModel
+                {
+                    User = user,
+                    Roles = roles
+                });
+            }
+            return View(userViewModels);
+        }
+    }
+}
---
+++ OnlineShop.Web/Areas/Admin/Models/UserViewModel.cs
@@ -0,0 +1,10 @@
+﻿using OnlineShop.Data.Models;
+
+namespace OnlineShop.Web.Areas.Admin.Models
+{
+    public class UserViewModel
+    {
+        public ApplicationUser User { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+}
