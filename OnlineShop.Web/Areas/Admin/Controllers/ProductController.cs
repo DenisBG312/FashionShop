7abc81ddd5da1c25ea67e720a27f71dc993ddd09
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2024-12-02 14:26:05 +0200

    moved the create a product logic in the admin area

--- OnlineShop.Web/Areas/Admin/Controllers/ProductController.cs
+++ OnlineShop.Web/Areas/Admin/Controllers/ProductController.cs
@@ -2,6 +2,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OnlineShop.Data;
+using OnlineShop.Services.Data;
+using OnlineShop.Services.Data.Interfaces;
+using OnlineShop.Web.ViewModels.Product;
+using System.Security.Claims;
 
 namespace OnlineShop.Web.Areas.Admin.Controllers
 {
@@ -10,15 +14,56 @@ namespace OnlineShop.Web.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly ApplicationDbContext _context;
-        public ProductController(ApplicationDbContext context)
+        private readonly IProductService _productService;
+        public ProductController(ApplicationDbContext context, IProductService productService)
         {
             _context = context;
+            _productService = productService;
         }
         public async Task<IActionResult> Index()
         {
             var products = await _context.Products.ToListAsync();
             
             return View(products);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+
+            var viewModel = new CreateProductViewModel
+            {
+                Genders = await _productService.GetGendersAsync(),
+                ClothingTypes = await _productService.GetClothingTypesAsync()
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductViewModel product)
+        {
+            ModelState.Remove(nameof(product.Genders));
+            ModelState.Remove(nameof(product.ClothingTypes));
+
+            if (!ModelState.IsValid)
+            {
+                product.Genders = await _productService.GetGendersAsync();
+                product.ClothingTypes = await _productService.GetClothingTypesAsync();
+
+                return View(product);
+            }
+
+            await _productService.CreateProductAsync(product, GetUserId()!);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+        public string? GetUserId()
+        {
+            return User.FindFirstValue(ClaimTypes.NameIdentifier);
         }
     }
 }
